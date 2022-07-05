af2b85d8c7f9223dd41103a590147721f0a40a3f
francescoP02 <file.partipilo@gmail.com>
2022-07-05 17:02:20 +0200

    Scaffolding front office with Vue

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 
-// Route::get('/test', 'Admin\HomeController@index')->name('home');
+//Route::get('/test', 'Admin\HomeController@index')->name('home');
 
 Route::middleware('auth')
     ->namespace('Admin')
@@ -30,3 +26,7 @@ Route::middleware('auth')
     ->group(function() {
         Route::get('/', 'HomeController@index')->name('home');
     });
+
+Route::get('{any?}', function() {
+    return view('guest.home');
+})->where('any', '.*');
