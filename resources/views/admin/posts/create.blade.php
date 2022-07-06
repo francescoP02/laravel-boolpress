4b41867e9caf4bc0e93eed1216a490270a1d079c
francescoP02 <file.partipilo@gmail.com>
2022-07-06 16:52:57 +0200

    Implemented create and store functions of Posts table with slug generation and controll

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <h1>Create a new post</h1>
+
+    <form action="{{ route('admin.posts.store') }}" method="POST">
+        @method('POST')
+        @csrf
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <div class="mb-3">
+            <label for="title" class="form-label">Title:</label>
+            <input type="text" class="form-control" name="title" id="title">
+        </div>
+        <div class="mb-3">
+            <label for="content" class="form-label">Content:</label>
+            <textarea type="text" class="form-control" name="content" id="content"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+@endsection
