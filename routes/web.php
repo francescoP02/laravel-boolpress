8cefd413fbd33ee2d25e62e834eebb1aeef36044
francescoP02 <file.partipilo@gmail.com>
2022-07-06 15:44:26 +0200

    Scaffolding resource controller of post table

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::middleware('auth')
     ->prefix('admin')
     ->group(function() {
         Route::get('/', 'HomeController@index')->name('home');
+        Route::resource('post', 'PostController');
     });
 
 Route::get('{any?}', function() {
