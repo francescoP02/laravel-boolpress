71deaf41f3cba006a5fce8bdb1ae42f057215d41
francescoP02 <file.partipilo@gmail.com>
2022-07-08 17:53:03 +0200

    Implemented tags table for delete function with soft delete

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -123,6 +123,7 @@ class PostController extends Controller
     public function destroy($id)
     {
         $post = Post::findOrFail($id);
+        $post->tags()->sync([]);
         $post->delete();
         return redirect()->route('admin.posts.index');
     }
